7e6cfed6978d812cdb8b4ddc460dcacc486064ec
Mark Young <markyaaronyoung@gmail.com>
2021-04-02 16:21:36 -0500

    added a registration form

---
+++ src/components/pages/SignUp/SignUp.js
@@ -0,0 +1,212 @@
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+import userData from '../../../helpers/data/userData';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        StruggleBus Assistant
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  const [email, setEmail] = useState('');
+
+  const [password, setPassword] = useState('');
+
+  const [firstName, setFirstName] = useState('');
+
+  const [lastName, setLastName] = useState('');
+
+  const [username, setUsername] = useState('');
+
+  const createUser = (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      password,
+      username,
+    };
+    const jsonUser = JSON.stringify(newUser);
+
+    userData.addUser(jsonUser)
+      .then((res) => {
+        if (res.data.valid === true) {
+          localStorage.setItem('authed', true);
+          localStorage.setItem('token', res.data.token);
+          localStorage.setItem('user_id', res.data.user_id);
+          this.props.authToggle();
+        } else {
+          console.error('incorrecct password and/or username');
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const emailUpdate = (e) => {
+    e.preventDefault();
+    setEmail(`${e.target.value}`);
+  };
+
+  const passwordUpdate = (e) => {
+    e.preventDefault();
+    setPassword(`${e.target.value}`);
+  };
+
+  const firstNameUpdate = (e) => {
+    e.preventDefault();
+    setFirstName(`${e.target.value}`);
+  };
+
+  const lastNameUpdate = (e) => {
+    e.preventDefault();
+    setLastName(`${e.target.value}`);
+  };
+
+  const usernameUpdate = (e) => {
+    e.preventDefault();
+    setUsername(`${e.target.value}`);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Join The Club
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+                onChange={firstNameUpdate}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                onChange={lastNameUpdate}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="userame"
+                label="username"
+                name="Username"
+                autoComplete="uname"
+                onChange={usernameUpdate}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={emailUpdate}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={passwordUpdate}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={createUser}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/home" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
